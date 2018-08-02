38f4551a1a626023954256884b538c86961ebcef
Satan7797 <36474735+Satan7797@users.noreply.github.com>
2018-08-02 22:05:57 +0530

    Update 7.cpp

--- Discrete Structure/7.cpp
+++ Discrete Structure/7.cpp
@@ -5,14 +5,16 @@ using namespace std;
 
 int binary(int a[],int first,int last,int x)	//function for Binary Search
 {
-	int mid=(first+last)/2;
-
-	if(a[mid]==x)				//if value if found at mid then function will return mid
-	  return mid;
-	else if(a[mid]>x)			//if value is less than value at mid then check in second half of array  
-	  return binary(a,first,mid-1,x);
-	else if(a[mid]<x)			//if value is greater than value at mid then check in first half of array
-	  return binary(a,mid+1,last,x);
+	if(x>=a[0]&&x<=a[last-1])
+	{
+	  int mid=(first+last)/2;
+	  if(a[mid]==x)				//if value if found at mid then function will return mid
+	   return mid;
+	  else if(a[mid]>x)			//if value is less than value at mid then check in second half of array  
+	   return binary(a,first,mid-1,x);
+	  else if(a[mid]<x)			//if value is greater than value at mid then check in first half of array
+	   return binary(a,mid+1,last,x);
+	}
 	else					//if number is not found than return -1
 	  return -1;
 }
