88e9c7a53e6f666e3c21d87d13c5b8ec89b9b79d
Satan7797 <srajbir070@gmail.com>
2018-10-04 22:45:58 +0530

    9DS

---
+++ Data Structure/9.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+	string exp;
+	double a,b;
+	stack<double> s;
+	cout<<"\nEnter the expression: ";
+	cin>>exp;
+	
+	for(int i=0;i<exp.length();i++)
+	if(exp[i]>=48 && exp[i]<=57)
+	s.push(exp[i]-'0');
+	else if(exp[i]>=42 && exp[i]<=47)
+	{
+		a=s.top();
+		s.pop();
+		b=s.top();
+		s.pop();
+		switch(exp[i])
+		{
+			case '+': s.push(a+b);
+					  break;
+			case '-': s.push(b-a);
+					  break;
+			case '*': s.push(a*b);
+					  break;
+			case '/': s.push(b/a);
+					  break;
+		}
+	}
+	else
+	cout<<"\nInvalid character "<<exp[i];
+	cout<<"\nAnswer is: "<<s.top();
+	s.pop();
+	return 0;
+}
