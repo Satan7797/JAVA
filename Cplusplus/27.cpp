f980231a59d9e30cf77acf9ef51f3e99da4c6a1a
Not-James-Bond <chaitanyasingh92@yahoo.com>
2018-11-11 01:33:14 +0530

    Same as 28.cpp

--- Cplusplus/27.cpp
+++ Cplusplus/27.cpp
@@ -1,43 +1,42 @@
 //Copy the contents of one text file to another file, after removing all whitespaces.
 
-//Not sure for this one.
-
 #include<iostream>
 #include<fstream>
+#include<cctype>
 using namespace std;
 
 int main()
 {
 	ifstream fin;
 	ofstream fout;
-	
+
 	char ch;
-	
+
 	fin.open("myfile.txt");
 	fout.open("temp.txt");
-	
+
 	if(!fin)
 	cout<<"file not exist \n";
-	
+
 	while(!fin.eof())
 	{
 		fin>>ch;
+		if(isalnum(ch) != 0 || isalpha(ch) != 0 )
 		fout<<ch;
 	}
-	
+
 	fin.close();
 	fout.close();
-	
+
 	fin.open("temp.txt");
-	
+
 	while(!fin.eof())
     {
 		fin.get(ch);
 	  cout<<ch;
     }
-	
+
 	fin.close();
- 
+
  return 0;
 }
-
