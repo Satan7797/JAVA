50b47fa9e2261d29945f98649357ea982e1cf95f
Not-James-Bond <chaitanyasingh92@yahoo.com>
2018-11-11 01:24:53 +0530

    Data File Handling FTW

--- Cplusplus/28.cpp
+++ Cplusplus/28.cpp
@@ -1,36 +1,32 @@
 //Copy the contents of one text file to another file, after removing all whitespaces.
+//Make one text file named as "1st_File.txt" with some content in it (in the directory of the CPP)
 
 #include<iostream>
+#include<fstream>
+#include<cctype>
 using namespace std;
-
-int ar[100];
-int SIZE;
-
-void reverse(int *f);
 
 int main()
 {
-	cout<<"enter size:";
-	cin>>SIZE;
-	cout<<"Enter array:";
-	for(int i=0;i<SIZE;i++)
-	cin>>ar[i];
-	reverse(ar);			//function call to reverse the array
-	cout<<"\nReversed array:";
-	for(int i=0;i<SIZE;i++)
-	cout<<ar[i]<<" ";
+	ifstream fin("1st_File.txt");
+	ofstream fout("2nd_File.txt");
+	if(!fin)
+	{
+		char c;
+		cout<<"\nNo File Found";
+		fin>>c;
+	}
+	else
+	{
+		char ch;
+			while(fin.eof()==0)
+			{
+				fin>>ch;
+				if(isalnum(ch) != 0 || isalpha(ch) != 0 )
+				fout<<ch;
+			}
+	}
+	fin.close();
+	fout.close();
 	return 0;
 }
-
-void reverse(int *f)		//function to reverse array
-{
-	int l=SIZE-1;
-	for(int i=0;i<SIZE/2;i++,l--)		//loop to interchange the elements of array
-	{
-		int temp=f[i];
-		f[i]=f[l];
-		f[l]=temp;
-	}
-}
-
-
