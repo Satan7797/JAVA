843c41cfe2151654a1261891a48526a4a04fb1d6
Satan7797 <36474735+Satan7797@users.noreply.github.com>
2018-11-25 03:06:48 +0530

    Add files via upload

---
+++ Network/HammingCodeFinal.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+bool posCheck(int i,int r)
+{
+	for(int j=0;j<=r;j++)
+	if(i==pow(2,j))
+	return false;
+	return true;
+}
+
+int main()
+{
+	int m,r;
+	cout<<"\nEnter m: ";
+	cin>>m;
+	
+	int data[m]={1,0,1,1,0,0,1};
+	
+	cout<<"\nEnter data: ";
+	for(int i=0;i<m;cin>>data[i],i++);
+	
+	for(r=1;;r++)
+	if((pow(2,r))>=(m+r+1))
+	break;
+
+	int sd=m+r,k=0,dataSent[sd],temp=r;
+	for(int i=sd-1;i>=0;i--)
+	{
+		if(posCheck(i+1,r))
+			dataSent[i]=data[k++];
+		else
+			dataSent[i]=temp--;
+	}
+	
+	for(int i=0;i<sd;i++)
+    if(!posCheck(i+1,r))
+	{
+		int sum=0,s=i+1;
+		for(int j=s;j<=sd;j++)
+		if((j&s)> 0 && j!=s)
+		  sum+=dataSent[j-1];
+		
+		///even parity check
+		if(sum%2==0)
+		  dataSent[i]=0;
+		else
+		  dataSent[i]=1;
+	}
+
+	cout<<"\nData Sent: ";
+	for(int i=sd-1;i>=0;i--)
+	cout<<dataSent[i];
+
+	cout<<"\nRedundant bits: ";
+	for(int i=0;i<sd;i++)
+    if(!posCheck(i+1,r))
+	cout<<dataSent[i];
+		
+	return 0;
+}
